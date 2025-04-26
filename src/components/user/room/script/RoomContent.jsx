cda3d920a39a170fae6bc06ccfe72df9f829cfe3
eziquexx <waftyann@gmail.com>
2025-04-27 04:03:54 +0900

    25.04.27 지은 : roomview 페이지 스타일 전체 수정

--- src/components/user/room/script/RoomContent.jsx
+++ src/components/user/room/script/RoomContent.jsx
@@ -30,23 +30,27 @@ export default function RoomContent() {
 
   return (
     <div className="RoomView_Top">
-    <nav className="RoomView_container">
-      <h2>RoomView</h2>
-      <div className="room-content">
-      <div className="room-header">
-        <h1>객실</h1>
-        <p>서울의 역사와 비즈니스의 중심 소공동에 위치한 HJ호텔은 최대의 규모를 갖춘 한국 최고의 럭셔리 호텔입니다.<br/>
-           서울 다운타운의 중심에서 남산과 명동, 스카이라인을 감상할 수 있는 롯데호텔 서울은 세계적인 인테리어 회사들이 참가하여 설계한 독창적인 객실을 갖추고 있으며 성공적인 비즈니스를 위한 최적의 공간입니다.</p>
-      </div>
-      <div className="room-type-list">
-        {/* roomTypes 배열을 map을 사용하여 각 객실 타입에 대해 RoomType 컴포넌트를 렌더링 */}
-        {roomTypes.map((roomType) => (
-          <RoomType key={roomType.roomTypeId} roomType={roomType} />
-        ))}
-      </div>
-    </div>
-     
-    </nav>
+      <nav className="RoomView_container">
+        <ul className="RoomView_list">
+          <li className="title">
+            <h2>RoomView</h2>
+          </li>
+          <li className="contents_wrap room-content">
+            <div className="room-header">
+              <p>
+                서울의 역사와 비즈니스의 중심 소공동에 위치한 HJ호텔은 최대의 규모를 갖춘 한국 최고의 럭셔리 호텔입니다.<br/>
+                서울 다운타운의 중심에서 남산과 명동, 스카이라인을 감상할 수 있는 롯데호텔 서울은<br/> 세계적인 인테리어 회사들이 참가하여 설계한 독창적인 객실을 갖추고 있으며 성공적인 비즈니스를 위한 최적의 공간입니다.
+              </p>
+            </div>
+            <div className="room-type-list">
+              {/* roomTypes 배열을 map을 사용하여 각 객실 타입에 대해 RoomType 컴포넌트를 렌더링 */}
+              {roomTypes.map((roomType) => (
+                <RoomType key={roomType.roomTypeId} roomType={roomType} />
+              ))}
+            </div>
+          </li>
+        </ul>
+      </nav>
     </div>
 
   );
