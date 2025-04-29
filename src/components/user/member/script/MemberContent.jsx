da1c818bb7cbbbe0b3853b7a996545bc0fe1bb42
eziquexx <waftyann@gmail.com>
2025-04-29 17:01:23 +0900

    25.04.29 지은 : login 페이지 전체 스타일 디테일 수정

--- src/components/user/member/script/MemberContent.jsx
+++ src/components/user/member/script/MemberContent.jsx
@@ -3,7 +3,6 @@ import { Outlet,Link } from "react-router-dom";
 export default function MemberContent() {
     return (
         <div style={{overflow:"hidden"}}>
-
             <Outlet/> {/* 하위 라우트를 렌더링 */}
         </div>
     );
